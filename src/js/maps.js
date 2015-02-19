e4626da3640065bda6d8b21210a411f9d5065273
RomuloCtba <romuloctba@gmail.com>
2015-02-18 23:14:56 -0200

    parte2

--- src/js/maps.js
+++ src/js/maps.js
@@ -1,5 +1,4 @@
 console.log('testando o arquivo :)');
-
 var inicial = {
 	latitude: -25.428777
 	, longitude: -49.3435829
@@ -12,5 +11,41 @@ var opcoes = {
 };
 
 function initialize() {
-        var map = new google.maps.Map(divDoMapa, opcoes);
-      }
+        map = new google.maps.Map(divDoMapa, opcoes);
+        }
+
+var imagens = {
+	muitoBom: 'http://i.imgur.com/bFnWq8k.png'
+	, bom: 'http://i.imgur.com/VnlbIoL.png'
+	, medio: 'http://i.imgur.com/eNAvIvr.png'
+	, ruim: 'http://i.imgur.com/uCRXqdV.png'
+	, pessimo: 'http://i.imgur.com/biRJBNL.png'
+}
+
+var marcadores = [];
+var criaMarcador = function(marcador, mapa) {
+  var posicao = new google.maps.LatLng(marcador.latitude, marcador.longitude);
+  var opcoes = {
+    position: posicao
+    , title: marcador.titulo
+    , animation: google.maps.Animation.DROP
+    , icon:{
+    		url: marcador.imagem
+    		, scaledSize: new google.maps.Size(50, 50)
+    	}
+    , map: mapa
+  }
+	var novoMarcador = new google.maps.Marker(opcoes);
+  marcadores.push(novoMarcador);
+  map.setCenter(novoMarcador.position)
+}
+
+function adiciona(){
+	var marcador = {
+		latitude: -25.425777
+		, longitude: -49.3335829	
+		, titulo: 'Novo marcador'
+		, imagem: imagens.muitoBom
+	}
+	criaMarcador(marcador, map);
+}
